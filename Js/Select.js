1d07780707b66acddd92ce9689381eeb2431c086
unknown <anik.fahim07@gmail.com>
2022-08-23 18:42:56 +0600

    first commit

---
+++ Js/Select.js
@@ -0,0 +1,46 @@
+document.getElementById('player-1').addEventListener('click',function(){
+   const playerName = document.getElementById('playerName-1');
+   const name = playerName.innerText;
+   const playerList = document.getElementById('showPlayer-1');
+   playerList.innerText = name;
+});
+// player 2
+document.getElementById('player-2').addEventListener('click',function(){
+const playerName = document.getElementById('playerName-2');
+const name = playerName.innerText;
+const playerList = document.getElementById('showPlayer-2');
+playerList.innerText = name;
+});
+ // player 3
+document.getElementById('player-3').addEventListener('click',function(){
+ 
+const playerName = document.getElementById('playerName-3');
+const name = playerName.innerText;
+const playerList = document.getElementById('showPlayer-3');
+playerList.innerText = name;
+});
+ // player 4
+ document.getElementById('player-4').addEventListener('click',function(){
+ 
+   const playerName = document.getElementById('playerName-4');
+   const name = playerName.innerText;
+   const playerList = document.getElementById('showPlayer-4');
+   playerList.innerText = name;
+   });
+ // player 5
+ document.getElementById('player-5').addEventListener('click',function(){
+ 
+   const playerName = document.getElementById('playerName-5');
+   const name = playerName.innerText;
+   const playerList = document.getElementById('showPlayer-5');
+   playerList.innerText = name;
+   });
+ // player 6
+ document.getElementById('player-6').addEventListener('click',function(){
+ 
+   const playerName = document.getElementById('playerName-6');
+   const name = playerName.innerText;
+   const playerList = document.getElementById('showPlayer-6');
+   playerList.innerText = name;
+   });
+   
